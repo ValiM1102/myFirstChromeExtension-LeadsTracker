579f68e3dc4e87e22f02fde8ad2bc0982b188a7c
Valentin <vmihaila333@gmail.com>
2023-02-08 21:26:35 +0200

    Needs some updates but will do for now

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # myFirstChromeExtension-LeadsTracker
-This is my first chrome extension, read the README file to learn more about it. Thanks and have a good day!
+This is my first chrome extension, read the README file to learn more about it. Thanks and have a good day! Coding along with Per from Scrimba.com in a freeCodeCamp video, all the other projects are in the learn.java.script but from now on I'll make each project individually. I've learned so much from this and I can't wait to continue on with programing.  
---
+++ js/index.js
@@ -0,0 +1,29 @@
+const inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.querySelector("#input-el")
+const unList = document.querySelector("#ul-el")
+
+
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    render()
+    inputEl.value = 'input here' 
+})
+
+function render(){
+    let listItems = ""
+    for (let i = 0; i<myLeads.length; i++){
+
+        listItems += `
+        <li>
+            <a href = '${myLeads[i]}' target = '_blank'>
+                ${myLeads[i]}
+            </a>
+        </li>`
+        
+    }
+    unList.innerHTML = listItems
+   
+}
