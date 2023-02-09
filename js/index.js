67033b8891f8aa7856a7435681ef38b88b750ef0
Valentin <vmihaila333@gmail.com>
2023-02-09 20:24:43 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,21 +1,48 @@
 const inputBtn = document.getElementById("input-btn")
-
 let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const unList = document.querySelector("#ul-el")
 const delBtn = document.querySelector("#del-btn")
-
 const leadsFromMyLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const saveBtn = document.querySelector("#save-btn") 
+
 
 if(leadsFromMyLocalStorage){
     myLeads = leadsFromMyLocalStorage 
-    render()
+    render(myLeads)
+}
+
+saveBtn.addEventListener("click", function(){
+
+    chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+        cmyLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+    
+    
+})
+
+function render(leads){
+    let listItems = ""
+    for (let i = 0; i<leads.length; i++){
+        listItems += `
+        <li class="list">
+            <a href = '${leads[i]}' target = '_blank'>
+                ${leads[i]}
+            </a>
+        </li>`
+        
+    }
+    unList.innerHTML = listItems
+   
 }
 
 delBtn.addEventListener("dblclick", function(){
     localStorage.clear()
     myLeads = []
-    render()
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function(){
@@ -26,17 +53,3 @@ inputBtn.addEventListener("click", function(){
 })
 
 
-function render(){
-    let listItems = ""
-    for (let i = 0; i<myLeads.length; i++){
-        listItems += `
-        <li class="list">
-            <a href = '${myLeads[i]}' target = '_blank'>
-                ${myLeads[i]}
-            </a>
-        </li>`
-        
-    }
-    unList.innerHTML = listItems
-   
-}
